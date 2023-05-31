829cc1511c84175d8ba86ee9a87b11c8f03e27fe
Yacdany Arguedas Mora <jack20-21@hotmail.com>
2023-05-31 08:37:40 -0600

    Adding Card styles

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,23 @@
 import Card from "../Components/Card/Card"
 
 const HomePage = () => {
+	const cont = new Array(10).fill(0)
+
 	return (
 		<div className="main">
 			<section>
-				<Card />
+				<div
+					style=
+					{{
+						display: "flex",
+						justifyContent: "center",
+						flexWrap: "wrap",
+						gap: "50px",
+						marginTop: "2.5rem"
+					}}
+				>
+					{cont.map((el) => <Card />)}
+				</div>
 			</section>
 		</div>
 	)
